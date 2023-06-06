98239435ba7f1f206df5ba2ac30221b4195cfb95
NavyaNelluri <123142678+NavyaNelluri@users.noreply.github.com>
2023-06-06 17:41:45 -0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -15,12 +15,11 @@ def send_email(api_key, to_email, from_email):
     )
 
     #try:
-     sg = sendgrid.SendGridAPIClient(api_key='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')
-     print("hello")
-     response = sg.send(message)
-     print('SendGrid API response:', response.status_code, response.body)
-
-     print('Email sent successfully')
+    sg = sendgrid.SendGridAPIClient(api_key='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')
+    print("hello")
+    response = sg.send(message)
+    print('SendGrid API response:', response.status_code, response.bo
+    print('Email sent successfully')
     #except Exception as e:
         #print('Error sending email:', str(e))
 
