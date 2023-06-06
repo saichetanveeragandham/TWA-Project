1c069005a4088a617945a71bfbf07644083a92c1
NavyaNelluri <123142678+NavyaNelluri@users.noreply.github.com>
2023-06-06 18:22:21 -0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -17,7 +17,7 @@ def send_email(api_key, to_email, from_email):
     )
 
     #try:
-    sg = sendgrid.SendGridAPIClient(api_key='nmMMUXFsQ7OE-OQhWgjYog')
+    sg = sendgrid.SendGridAPIClient(api_key='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')
     print("hello")
     response = sg.send(message)
     print('SendGrid API response:', response.status_code, response.body)
