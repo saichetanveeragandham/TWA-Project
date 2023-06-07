a60f5307cb3cbb4f670b0db9ad14491e63b21d33
saichetanveeragandham <120769407+saichetanveeragandham@users.noreply.github.com>
2023-06-06 21:21:18 -0500

    Update TWA.py

--- TWA.py
+++ TWA.py
@@ -2,6 +2,8 @@
 import snowflake.connector
 from tkinter import *
 #connection to database established with credentials
+
+
 try:
     con_eb = snowflake.connector.connect(user='NavyaNelluri',
                                      password='Navya.c@698',
