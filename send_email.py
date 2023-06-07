5649872b269f966f5f5b8cd185f64ec6feafab2f
NavyaNelluri <123142678+NavyaNelluri@users.noreply.github.com>
2023-06-06 19:45:16 -0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -6,7 +6,7 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 def send_email(api_key, to_email, from_email):
     print("hi");
     message = Mail(
-        from_email="navya.nelluri98@gmail.com",
+        from_email="navyachowdary.nelluri@slu.edu",
         to_emails=["navyachowdary.nelluri@slu.edu","saichetan.veeragandham@slu.edu"],
         subject='TWA Notification: Push event on main branch',
         plain_text_content=''' A push event has occurred on the main branch.
